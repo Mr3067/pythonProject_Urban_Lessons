51d71ae0fdfdbdf7e8439c5ace7e2f58ed304164
Danilov Alex <danilov.alexandr@hotmail.com>
2024-03-04 17:42:11 +0300

    Добавлено выполнение практического занятия 11 по уроку "Специальные методы классов" Домашнее задание по уроку "Пространство имен"

---
+++ 11 2023-10-27 Домашняя работа по уроку Специальные методы классов.py
@@ -0,0 +1,76 @@
+"""
+https://urban-university.ru/members/courses/course999421818026/20231027-0000domasnaa-rabota-po-uroku-specialnye-metody-klassov-854615870903
+
+ПРАКТИЧЕСКОЕ ЗАДАНИЕ
+2023/10/27 00:00|Домашняя работа по уроку "Специальные методы классов"
+Домашнее задание по уроку "Пространство имен"
+
+Создайте новый проект в PyCharm
+Запустите созданный проект
+Ваша задача:
+Создайте новый класс House
+Создайте инициализатор для класса House, который будет задавать атрибут этажности self.numberOfFloors = 0
+Создайте метод setNewNumberOfFloors(floors), который будет изменять атрибут numberOfFloors на параметр floors и
+выводить в консоль numberOfFloors
+Полученный код напишите в ответ к домашему заданию
+
+
+"""
+
+class House:
+    def decoratorNunberFloor(func):
+
+        if func.__name__ == '__init__':
+            outstr = 'init has been run'
+
+        elif func.__name__ == 'setNewNumberOfFloors':
+            outstr = 'set has been change NewNumberOfFloors attribute'
+
+
+        def inner(*args, **kwargs):
+            if args[1] < 0:
+                print('\nNumber of floors couldn\'t be below zero\n\n')
+                func(*args, **kwargs)
+            #                 return 0
+            # exit('!!!!!   Wrong data      !!!!!')
+
+            if args[1] == 0:
+                print(f'Number of floors is {args[1]} when '+outstr)
+                func(*args, **kwargs)
+            elif args[1] == 1:
+                print(f'Number of floors is {args[1]} when '+outstr)
+                func(*args, **kwargs)
+            else:
+                print(f'Number of floors are {args[1]} when '+outstr)
+                func(*args, **kwargs)
+
+        return inner
+
+
+    @decoratorNunberFloor
+    def __init__(self,  numberOfFloors = 0):
+
+        self.NewNumberOfFloors = numberOfFloors
+
+    @decoratorNunberFloor
+    def setNewNumberOfFloors(self, floors):
+
+        self.NewNumberOfFloors = floors
+
+
+    #
+    # def __getitem__(self, item):
+    #
+    # def __setitem__(self, key, value):
+    #
+    # def __del__(self):
+    #
+    #
+    # def __delitem__(self, key):
+    #
+    # def __str__(self):
+    #
+    # def __repr__(self):
+
+
+
