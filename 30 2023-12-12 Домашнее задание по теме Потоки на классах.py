7a42a9d3488eadc56a474220c8f09120927d1790
Danilov Alex <danilov.alexandr@hotmail.com>
2024-05-12 20:56:55 +0300

    Добавлено выполнение практического занятия 30 по уроку Блокировки потоков для доступа к общим данным.

--- 30 2023-12-12 Домашнее задание по теме Потоки на классах.py
+++ 30 2023-12-12 Домашнее задание по теме Потоки на классах.py
@@ -20,25 +20,21 @@ class Knight(threading.Thread):
         self.lock2 = threading.Lock()
 
     def run(self):
-        self.lock.acquire()
         print(f'{self.name},на нас напали!', flush=True)
-        self.lock.release()
         while self.enemy > 0:
             time.sleep(random.random()/(random.random()+1))
-            self.lock.acquire()
-            print(21,self.lock2.acquire())
-            print(22, self.lock2.release())
             self.enemy -= self.skill
             self.days += 1
-            print(f'{self.name},сражается 1 день(дня)..., осталось {self.enemy} воинов.', flush=True)
-            self.lock.release()
+            print(f'{self.name},сражается {self.days} день(дня)..., осталось {self.enemy} воинов.', flush=True)
+            # self.lock.release()
         print(f'{self.name},одержал победу спустя {self.days} дней!', flush=True)
+        print(f'Все битвы закончились!')
 
 
-knight1 = Knight("Sir Lancelot", 10)  # Низкий уровень умения
-knight2 = Knight("Sir Galahad", 20)  # Высокий уровень умения
+knight1 = Knight("Sir Lancelot", 2)  # Низкий уровень умения
+knight2 = Knight("Sir Galahad", 5)  # Высокий уровень умения
 knight1.start()
 knight2.start()
 knight1.join()
 knight2.join()
-print(f'Все битвы закончились!')
+
