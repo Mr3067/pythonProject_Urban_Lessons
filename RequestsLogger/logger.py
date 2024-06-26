1cea7308c4390167143ad5bd9a14a7f6bb23cd06
Danilov Alex <danilov.alexandr@hotmail.com>
2024-06-26 17:29:42 +0300

    Added logger file1

--- RequestsLogger/logger.py
+++ RequestsLogger/logger.py
@@ -2,3 +2,52 @@
 
 
 """
+import logging
+#
+# my_logger = logging.getLogger('mylogger')
+# handler1 = logging.FileHandler(filename='success_responses.log',
+#                                mode='w',
+#                                encoding='utf-8')
+# handler1.setLevel(logging.INFO)
+# my_logger.addHandler(handler1)
+#
+# handler2 = logging.FileHandler(filename='bad_responses.log',
+#                                mode='w',
+#                                encoding='utf-8')
+# handler2.setLevel(logging.WARNING)
+# my_logger.addHandler(handler2)
+
+
+
+log_info = logging.getLogger(f'Log_INFO')
+log_info.setLevel(logging.INFO)
+fh_info = logging.FileHandler(
+    filename='success_responses.log',
+    mode='w',
+    encoding='utf-8')
+formatter = logging.Formatter(f'%(levelname)s:%(message)s')
+fh_info.setFormatter(formatter)
+log_info.addHandler(fh_info)
+
+
+log_warning = logging.getLogger('Log_WARNING')
+log_warning.setLevel(logging.WARNING)
+fh_warning = logging.FileHandler(
+    filename='bad_responses.log',
+    mode='w',
+    encoding='utf-8')
+formatter = logging.Formatter(f'%(levelname)s:%(message)s')
+fh_warning.setFormatter(formatter)
+log_warning.addHandler(fh_warning)
+
+
+log_error = logging.getLogger('Log_ERROR')
+log_error.setLevel(logging.ERROR)
+fh_error = logging.FileHandler(
+    filename='blocked_responses.log',
+    mode='w',
+    encoding='utf-8')
+formatter = logging.Formatter(f'%(levelname)s:%(message)s')
+fh_error.setFormatter(formatter)
+log_error.addHandler(fh_error)
+
---
+++ RequestsLogger/main.py
@@ -0,0 +1,31 @@
+import requests as rq
+import logging
+
+import logger
+
+
+
+sites = ['https://www.youtube.com/', 'https://instagram.com', 'https://wikipedia.org', 'https://yahoo.com',
+         'https://yandex.ru', 'https://whatsapp.com', 'https://twitter.com', 'https://amazon.com', 'https://tiktok.com',
+         'https://www.ozon.ru']
+
+for site in sites:
+    try:
+        response = rq.get(site, timeout=3)
+        logging.root.setLevel(logging.INFO)
+        if response.status_code == 200:
+            logger.log_info.info(f'%s   code %s',
+                                 site,
+                                 response.status_code)
+        else:
+            logging.root.setLevel(logging.WARNING)
+            logger.log_warning.warning(f'%s   code %s',
+                                 site,
+                                 response.status_code)
+    except Exception:
+        logging.root.setLevel(logging.ERROR)
+        logger.log_error.error(f'%s   code %s',
+                                   site,
+                                   response.status_code)
+
+
