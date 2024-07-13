3b1d97ce1a6613e7d0547886643e9de13c233e5f
Danilov Alex <danilov.alexandr@hotmail.com>
2024-07-13 09:34:50 +0300

    Добавлено выполнение Домашнее задание по теме "Регулярные выражения"

---
+++ 41 Домашнее задание по теме Регулярные выражения .py
@@ -0,0 +1,25 @@
+"""
+https://habr.com/ru/articles/349860/
+https://habr.com/ru/companies/skillfactory/articles/677402/
+
+
+"""
+import re
+
+
+def extract_image_links(html_text):
+    match = re.findall(r'img src=\'(http[s]?:[\w\./]*)',
+                       html_text)
+    return match
+
+
+sample_html = "<img src='https://example.com/image1.jpg'> <img src='http://example.com/image2.png'> <img src='https://example.com/image3.gif'>"
+
+image_links = extract_image_links(sample_html)
+if image_links:
+    for image_link in image_links:
+        print(image_link)
+else:
+    print("Нет ссылок с картинками в HTML тексте.")
+
+
