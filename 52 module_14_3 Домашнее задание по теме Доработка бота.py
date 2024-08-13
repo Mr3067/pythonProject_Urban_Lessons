970a3ef6442dfcb192a2b4fe3a1bc84919b29b92
Danilov Alex <danilov.alexandr@hotmail.com>
2024-08-13 20:32:57 +0300

    Добавлено выполнение Домашнее задание по теме Домашнее задание по теме "Доработка бота".

---
+++ 52 module_14_3 Домашнее задание по теме Доработка бота.py
@@ -0,0 +1,145 @@
+"""
+версия: aiogram 3.11.0
+
+учебный бот: https://t.me/module_14_3_bot
+
+"""
+from Data_Bot import api_module_14_3 as api
+import asyncio, os
+
+from aiogram import F, Bot, Dispatcher
+from aiogram.types import (Message, InlineKeyboardButton, CallbackQuery, FSInputFile)
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.filters import Command
+
+bot = Bot(token=api)
+dp = Dispatcher(storage=MemoryStorage())
+
+
+class SomeStates(StatesGroup):
+    state1 = State()
+    state2 = State()
+    state3 = State()
+    state4 = State()
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: Message, state: FSMContext):
+    builder_start = InlineKeyboardBuilder()
+    builder_start.add(
+        InlineKeyboardButton(text="Рассчитать норму калорий", callback_data="calories"),
+        InlineKeyboardButton(text="Формулы расчёта", callback_data="formulas"),
+        InlineKeyboardButton(text="Купить", callback_data="buying_4_products")
+    )
+    builder_start.adjust(2)
+    await message.answer(
+        text='Привет! Я бот, помогающий твоему здоровью.',
+        reply_markup=builder_start.as_markup()
+    )
+    await state.set_state(SomeStates.state1)
+
+
+@dp.callback_query(F.data == "formulas")
+async def msg_formulas(callback: CallbackQuery, state: FSMContext):
+    builder_back = InlineKeyboardBuilder()
+    builder_back.add(
+        InlineKeyboardButton(text="Назад в меню", callback_data="main_menu")
+    )
+    await callback.message.answer(text=f'для мужчин: 10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5;\n'
+                                       f'для женщин: 10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161',
+                                  reply_markup=builder_back.as_markup()
+                                  )
+
+
+@dp.callback_query(F.data == "main_menu")
+async def cmd_start(callback: CallbackQuery):
+    builder_start = InlineKeyboardBuilder()
+    builder_start.add(
+        InlineKeyboardButton(text="Рассчитать норму калорий", callback_data="calories"),
+        InlineKeyboardButton(text="Формулы расчёта", callback_data="formulas"),
+        InlineKeyboardButton(text="Купить", callback_data="buying_4_products")
+    )
+    builder_start.adjust(2)
+    await callback.message.answer(
+        text='Привет! Я бот, помогающий твоему здоровью.',
+        reply_markup=builder_start.as_markup()
+    )
+
+
+@dp.callback_query(F.data == "buying_4_products")
+async def buying_4_products(callback: CallbackQuery):
+    buyng_4_products = InlineKeyboardBuilder()
+    buyng_4_products.add(
+        InlineKeyboardButton(text="Product1", callback_data='product_buying'),
+        InlineKeyboardButton(text="Product2", callback_data='product_buying'),
+        InlineKeyboardButton(text="Product3", callback_data='product_buying'),
+        InlineKeyboardButton(text="Product4", callback_data='product_buying'),
+    )
+    for filename in os.listdir('Img_module_14_3'):
+        photo = FSInputFile('Img_module_14_3' + '\\' + filename)
+        await callback.message.answer_photo(
+            photo=photo,
+            caption=f'{filename.split('.')[0]} '
+                    f'| Описание: описание {filename.split('.')[0][-1]} '
+                    f'| Цена: {int(filename.split('.')[0][-1]) * 100}'
+        )
+    await callback.message.answer(
+        text='Выберите продукт для покупки:',
+        reply_markup=buyng_4_products.as_markup()
+    )
+
+@dp.callback_query(F.data == 'product_buying')
+async def send_confirm_message(callback: CallbackQuery):
+    await callback.message.answer(text="Вы успешно приобрели продукт!")
+
+
+
+@dp.callback_query(F.data == "calories")
+async def msg_Calories(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer(text=f'Введите свой возраст:')
+    await state.set_state(UserState.age)
+
+
+@dp.message(UserState.age)
+async def msg_growth(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await bot.send_message(message.chat.id,
+                           text=f'Введите свой рост:')
+    await state.set_state(UserState.growth)
+
+
+@dp.message(UserState.growth)
+async def msg_weight(message: Message, state: FSMContext):
+    await state.update_data(growth=message.text)
+    await bot.send_message(message.chat.id,
+                           text=f'Введите свой вес:')
+    await state.set_state(UserState.weight)
+
+
+@dp.message(UserState.weight)
+async def msg_calcul(message: Message, state: FSMContext):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    summery_m = 10 * int(data['weight']) + 6.25 * int(data['growth']) + 5 * int(data['age']) + 5
+    summery_w = 10 * int(data['weight']) + 6.25 * int(data['growth']) + 5 * int(data['age']) - 161
+    await bot.send_message(message.chat.id,
+                           text=f'Для мужчины норма калорий {summery_m}\n'
+                                f'Для женщины норма калорий {summery_w}')
+
+
+# ____________________________________________________________________
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
