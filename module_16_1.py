a3b741547346abf4e1953a5569738e5d9df5da85
Danilov Alex <danilov.alexandr@hotmail.com>
2024-08-27 22:00:29 +0500

    Добавлено выполнение Домашнее задание по теме Основы Fast Api и маршрутизация.

---
+++ module_16_1.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def main() -> str:
+    return ("Главная страница")
+
+@app.get("/user/admin")
+async def admin_pg() -> str:
+    return ("Вы вошли как администратор")
+
+@app.get("/user/{user_id}")
+async def user_id(user_id) -> str:
+    return (f"Вы вошли как пользователь № {user_id}")
+
+@app.get("/user")
+async def user_all(username:str ='Иван', age:int =25) ->dict:
+    return ({"User": username, "Age": age})
