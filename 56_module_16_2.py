43718fa4ad149403911d6d197359f8129a335848
Danilov Alex <danilov.alexandr@hotmail.com>
2024-08-28 21:34:43 +0500

    Добавлено выполнение Домашнее задание по теме Валидация данных.

--- 56_module_16_2.py
+++ 56_module_16_2.py
@@ -11,18 +11,18 @@ async def main() -> str:
     return ("Главная страница")
 
 
-# @app.get("/user/admin")
-# async def admin_pg() -> str:
-#     return ("Вы вошли как администратор")
-#
-#
-# @app.get("/user/{user_id}")
-# async def user_id(user_id) -> str:
-#     return (f"Вы вошли как пользователь № {user_id}")
+@app.get("/user/admin")
+async def admin_pg() -> str:
+    return ("Вы вошли как администратор")
 
 
-@app.get("/user/{username}/{id}")
-# async def user_all(username: str = Path(min_length=2, max_length=20, description="Введите свое имя"),
-#                    age: int = Path(ge=0, le=100, description="введите свой Id")) -> dict:
-async def user_all(username: str, age: int ) -> dict:
+@app.get("/user/{user_id}")
+async def user_id(user_id: int = Path(ge=1, le=100, description='Enter User ID',example=1)) -> str:
+    return (f"Вы вошли как пользователь № {user_id}")
+
+
+@app.get("/user/{username}/{age}")
+async def user_all(username: str = Path(min_length=5, max_length=20, description="Enter username", example='UrbanUser'),
+                   age: int = Path(ge=18, le=120, description="Enter age", example=24)) -> dict:
+
     return {"User": username, "Age": age}
