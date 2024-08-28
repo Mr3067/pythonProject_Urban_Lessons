39fbb2a2a572fe4add5847737dacfc58a29ede6b
Danilov Alex <danilov.alexandr@hotmail.com>
2024-08-28 21:50:46 +0500

    Добавлено выполнение Домашнее задание по теме Валидация данных.

--- 56_module_16_2.py
+++ 56_module_16_2.py
@@ -17,12 +17,12 @@ async def admin_pg() -> str:
 
 
 @app.get("/user/{user_id}")
-async def user_id(user_id: int = Path(ge=1, le=100, description='Enter User ID',example=1)) -> str:
+async def user_id(user_id: Annotated[int,Path(ge=1, le=100, description='Enter User ID',example=1)]) -> str:
     return (f"Вы вошли как пользователь № {user_id}")
 
 
 @app.get("/user/{username}/{age}")
-async def user_all(username: str = Path(min_length=5, max_length=20, description="Enter username", example='UrbanUser'),
-                   age: int = Path(ge=18, le=120, description="Enter age", example=24)) -> dict:
+async def user_all(username: Annotated[str,Path(min_length=5, max_length=20, description="Enter username", example='UrbanUser')],
+                   age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=24)]) -> dict:
 
     return {"User": username, "Age": age}
